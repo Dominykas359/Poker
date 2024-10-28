fbed0b15182a2da36be670bbd9d4f3b0686fb8b0
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-10-28 14:35:47 +0200

    created Deck class

--- src/models/Deck.java
+++ src/models/Deck.java
@@ -1,2 +1,40 @@
-package models;public class Deck {
+package models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+
+    private final List<Card> deck = new ArrayList<>();
+
+    public Deck(){
+        initializeDeck();
+    }
+
+    public void initializeDeck(){
+
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= 13; j++){
+                deck.add(new Card(i, j));
+            }
+        }
+
+        shuffleDeck();
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(deck);
+    }
+
+    public Card dealCard() {
+        if (!deck.isEmpty()) {
+            return deck.removeLast();
+        }
+        return null; // Returns null if no cards are left
+    }
+
+    public void resetDeck() {
+        initializeDeck(); // Resets and reshuffles the deck
+    }
 }
