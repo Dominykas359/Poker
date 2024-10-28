6eb5ffff30ac13d091afe8f86ca846a8537693f1
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-10-28 14:37:35 +0200

    crated Player class

--- src/models/Player.java
+++ src/models/Player.java
@@ -1,2 +1,39 @@
-package models;public class Player {
+package models;
+
+import java.util.List;
+
+public class Player {
+
+    private String name;
+    private int money;
+    private List<Card> cards;
+
+    public Player(String name, int money){
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
 }
