6b29d3bfb0cf64d3250aafb915caee85e291cace
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-10-29 17:56:07 +0200

    new fields to player class

--- src/models/Player.java
+++ src/models/Player.java
@@ -5,12 +5,23 @@ import java.util.List;
 public class Player {
 
     private String name;
-    private int money;
+    private double money;
+    private boolean smallBlind;
+    private boolean bigBlind;
+    private boolean move;
+    private boolean folded;
+    private double bet;
+    private boolean lost;
     private List<Card> cards;
 
-    public Player(String name, int money){
+    public Player(String name, double money, boolean smallBlind, boolean bigBlind, double bet, boolean folded, boolean lost){
         this.name = name;
         this.money = money;
+        this.smallBlind = smallBlind;
+        this.bigBlind = bigBlind;
+        this.bet = bet;
+        this.folded = folded;
+        this.lost = lost;
     }
 
     public String getName() {
@@ -21,11 +32,11 @@ public class Player {
         this.name = name;
     }
 
-    public int getMoney() {
+    public double getMoney() {
         return money;
     }
 
-    public void setMoney(int money) {
+    public void setMoney(double money) {
         this.money = money;
     }
 
@@ -35,5 +46,53 @@ public class Player {
 
     public void setCards(List<Card> cards) {
         this.cards = cards;
+    }
+
+    public boolean isSmallBlind() {
+        return smallBlind;
+    }
+
+    public void setSmallBlind(boolean smallBlind) {
+        this.smallBlind = smallBlind;
+    }
+
+    public boolean isBigBlind() {
+        return bigBlind;
+    }
+
+    public void setBigBlind(boolean bigBlind) {
+        this.bigBlind = bigBlind;
+    }
+
+    public boolean isMove() {
+        return move;
+    }
+
+    public void setMove(boolean move) {
+        this.move = move;
+    }
+
+    public double getBet() {
+        return bet;
+    }
+
+    public void setBet(double bet) {
+        this.bet = bet;
+    }
+
+    public boolean isFolded() {
+        return folded;
+    }
+
+    public void setFolded(boolean folded) {
+        this.folded = folded;
+    }
+
+    public boolean isLost() {
+        return lost;
+    }
+
+    public void setLost(boolean lost) {
+        this.lost = lost;
     }
 }
