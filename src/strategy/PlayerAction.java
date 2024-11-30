4216d0f680e70670405a046317c6fb998acf84e7
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-11-30 11:02:55 +0200

    centralizing usage of MoveStrategy

--- src/strategy/PlayerAction.java
+++ src/strategy/PlayerAction.java
@@ -1,2 +1,23 @@
-package strategy;public class PlayerAction {
+package strategy;
+
+import misc.Game;
+import misc.Table;
+import models.Player;
+
+import java.util.Scanner;
+
+public class PlayerAction {
+    private MoveStrategy moveStrategy;
+
+    public void setMoveStrategy(MoveStrategy moveStrategy) {
+        this.moveStrategy = moveStrategy;
+    }
+
+    public void performMove(Table table, Player player, Scanner scanner, Game game) {
+        if (moveStrategy != null) {
+            moveStrategy.execute(table, player, scanner, game);
+        } else {
+            System.out.println("No strategy selected!");
+        }
+    }
 }
