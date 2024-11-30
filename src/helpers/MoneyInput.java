8317bac7f9b9e8f372b0e093800a008e8db7441b
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-11-30 13:29:10 +0200

    helper function to take money input from user

--- src/helpers/MoneyInput.java
+++ src/helpers/MoneyInput.java
@@ -1,2 +1,29 @@
-package helpers;public class MoneyInput {
+package helpers;
+
+import java.util.Scanner;
+
+public class MoneyInput {
+
+    public static double inputMoney(Scanner scanner){
+
+        double money;
+
+        System.out.print("Enter the amount of money you want to play with: ");
+        while (true) {
+            if (scanner.hasNextDouble()) {
+                money = scanner.nextDouble();
+                scanner.nextLine();  // Clear the newline character
+                if (money > 0) {
+                    break;
+                } else {
+                    System.out.print("Please enter a positive amount: ");
+                }
+            } else {
+                System.out.print("Invalid input. Enter a valid amount: ");
+                scanner.next(); // Clear invalid input
+            }
+        }
+
+        return money;
+    }
 }
