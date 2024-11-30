c9dc5ee5869ad08fe692d543b321595db79704db
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-11-30 11:02:27 +0200

    implementing MoveStrategy with different poker actions

--- src/strategy/AllInStrategy.java
+++ src/strategy/AllInStrategy.java
@@ -1,2 +1,23 @@
-package strategy;public class AllInStrategy {
+package strategy;
+
+import misc.Game;
+import misc.Table;
+import models.Player;
+
+import java.util.Scanner;
+
+public class AllInStrategy implements MoveStrategy{
+    @Override
+    public void execute(Table table, Player player, Scanner scanner, Game game) {
+        table.setCurrentBet(player.getBet() + player.getMoney());
+        table.setPrizeMoney(table.getPrizeMoney() + player.getMoney());
+        player.setBet(player.getBet() + player.getMoney());
+        player.setMoney(0);
+        game.setFlop(false);
+        game.setTurn(false);
+        game.setRiver(false);
+        if (player.getMoney() <= 0) player.setFolded(true);
+        player.setMoved(true);
+        table.addActionHistory(STR."\{player.getName()} went all in.");
+    }
 }
--- src/strategy/RaiseStrategy.java
+++ src/strategy/RaiseStrategy.java
@@ -1,2 +1,36 @@
-package strategy;public class RaiseStrategy {
+package strategy;
+
+import misc.Game;
+import misc.Table;
+import models.Player;
+
+import java.util.Scanner;
+
+public class RaiseStrategy implements MoveStrategy{
+    @Override
+    public void execute(Table table, Player player, Scanner scanner, Game game) {
+        System.out.print("Enter amount of money you want to raise: ");
+        double raisedMoney;
+        while (true) {
+            if (scanner.hasNextDouble()) {
+                raisedMoney = scanner.nextDouble();
+                scanner.nextLine(); // Clear newline
+                if (raisedMoney > 0 && raisedMoney <= player.getMoney()) {
+                    break;
+                } else {
+                    System.out.print("Please enter a valid amount: ");
+                }
+            } else {
+                System.out.print("Invalid input. Enter a valid amount: ");
+                scanner.next(); // Clear invalid input
+            }
+        }
+        player.setBet(player.getBet() + raisedMoney);
+        table.setCurrentBet(player.getBet()); // Update current bet to player's total bet
+        table.setPrizeMoney(table.getPrizeMoney() + raisedMoney);
+        player.setMoney(player.getMoney() - raisedMoney);
+        if (player.getMoney() <= 0) player.setFolded(true);
+        player.setMoved(true);
+        table.addActionHistory(STR."\{player.getName()} raised $ \{raisedMoney}.");
+    }
 }
